a8c1cade0821da211d5cc6a425cc37cc162301d5
Thomas Sauques <39383180+Morphyish@users.noreply.github.com>
2019-12-26 17:22:02 +0100

    :sparkles: Added a button to start the game

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,15 @@
-Svelte
+<script>
+    import Button from './components/Button.svelte'
+    import Game from './Game.svelte'
+
+    let started = false
+</script>
+
+<Button
+        ariaLabel="Start"
+        on:click={() => started  = true}
+>
+    Start
+</Button>
+
+<Game {started} />
---
+++ src/Game.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let started = false
+</script>
+
+<div id="game" class:started></div>
+
+<style>
+    #game {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        height: 0;
+        width: 0;
+        overflow: hidden;
+        background: var(--primary);
+        transition: none;
+    }
+
+    #game.started {
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        transition: top 225ms ease,
+        left 225ms ease,
+        height 225ms ease,
+        width 225ms ease;
+    }
+</style>
